a75a92364c2585a71e06bad8ccb02e212512dca7
LcErhel <132855195+LcErhel@users.noreply.github.com>
2023-06-06 13:30:45 +0200

    Functionality fully done

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 "use strict"
 
 function getPlayerChoice() {
-    let choice = prompt("Rock/Paper/Scissor? || 0/1/2")
+    let choice = prompt("Rock/Paper/Scissor? || 0/1/2");
     if (choice.toLowerCase() == "rock" || choice == 0) {
         return 0;
     } else if (choice.toLowerCase() == "paper" || choice == 1) {
@@ -14,40 +14,71 @@ function getPlayerChoice() {
 }
 
 function getComputerChoice() {
-    let result = Math.floor(Math.random() * 3);
-    if (result == 0) {
-        return 0;
-    } else if (result == 1) {
-        return 1;
-    } else {
-        return 2;
-    }
+    return Math.floor(Math.random() * 3);
 }
 
 function playRound() {
     let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice()
+    let computerChoice = getComputerChoice();
+
     if (playerChoice == 0 && computerChoice == 0) {
-        return "Draw! rock/rock";
+        console.log("Draw! rock/rock");
+        return 2;
     } else if (playerChoice == 1 && computerChoice == 1) {
-        return "Draw! paper/paper";
+        console.log("Draw! paper/paper");
+        return 2;
     } else if (playerChoice == 2 && computerChoice == 2) {
-        return "Draw! scissors/scissors";
+        console.log("Draw! scissors/scissors")
+        return 2;
     } else if (playerChoice == 0 && computerChoice == 2) {
-        return "You win! rock/scissors";
+        console.log("You win! rock/scissors");
+        return 1;
     } else if (playerChoice == 1 && computerChoice == 0) {
-        return "You win! paper/rock";
+        console.log("You win! paper/rock");
+        return 1;
     } else if (playerChoice == 2 && computerChoice == 1) {
-        return "You win! scissors/paper";
+        console.log("You win! scissors/paper");
+        return 1;
     } else if (playerChoice == 2 && computerChoice == 0) {
-        return "You lose! scissors/rock";
+        console.log("You lose! scissors/rock");
+        return 0;
     } else if (playerChoice == 0 && computerChoice == 1) {
-        return "You lose! rock/paper";
+        console.log("You lose! rock/paper");
+        return 0;
     } else if (playerChoice == 1 && computerChoice == 2) {
-        return "You lose! paper/scissors";
+        console.log("You lose! paper/scissors");
+        return 0;
     } else {
         console.error("broken playRound()");
     }
 }
 
-console.log(playRound());
+function playFullGame() {
+    let checkResult;
+    let rounds = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let scoreSum = 5; scoreSum > (playerScore + computerScore);) {
+        checkResult = playRound();
+        ++rounds;
+        if (checkResult == 0) {
+            ++computerScore;
+        } else if (checkResult == 1) {
+            ++playerScore;
+        }
+
+        console.log(`Rounds: ${rounds}`);
+        console.log(`Score: ${playerScore} / ${computerScore}`);
+
+        if (playerScore >= 3) {
+            console.log("You've won!!!")
+            return;
+        } else if (computerScore >= 3) {
+            console.log("You've lost!!!")
+            return;
+        }
+    }
+}
+
+playFullGame();
