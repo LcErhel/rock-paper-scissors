273e33d9eab23b1ec8e1b5794d325aee309788e2
project <132855195+LcErhel@users.noreply.github.com>
2025-03-08 00:38:43 +0300

    add playRound function

--- script.js
+++ script.js
@@ -5,12 +5,41 @@ let computerScore = 0;
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    console.log(computerChoice);
     return computerChoice;
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("1 - rock, 2 - paper, 3 - scissors");
-    console.log(humanChoice);
     return humanChoice;
+}
+
+function playRound(computerChoice, humanChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    console.log(computerChoice, humanChoice);
+
+    if(!Number.isInteger(+humanChoice)) {
+        return console.log("not a number")
+    }
+    
+    if(humanChoice >= 4 || humanChoice <= 0) {
+        return console.log("invalid number");
+    }
+
+    if(computerChoice == humanChoice) {
+        console.log("draw");
+    } else if (computerChoice == 1 && humanChoice == 2) {
+        console.log("win");
+        humanScore++;
+    } else if (computerChoice == 2 && humanChoice == 3) {
+        console.log("win");
+        humanScore++;
+    } else if (computerChoice == 3 && humanChoice == 1) {
+        console.log("win");
+        humanScore++;
+    } else {
+        computerScore++;
+        console.log("lose");
+    }
+    console.log(computerScore, humanScore);
 }
